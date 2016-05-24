ef2fea6c9f9494e9b035d02fe29c8c14c47a5452
Tarik Bennett <tarik.bennett@gmail.com>
2016-05-23 19:48:05 -0700

    Add HTML via OOJS

--- assets/js/mypopup.js
+++ assets/js/mypopup.js
@@ -3,11 +3,47 @@ setTimeout(openSignUpBox, 3000);
 
 // Display signup box after initial pause
 function openSignUpBox() {
+
+  createPopUp();
+
   document.getElementById('modal-shadow').style.display = "block";
+
 }
 
 
 // Construct SignUpBox
+
+
+function createPopUp() {
+
+  var container = document.getElementById('signUpBox');
+
+  var form = document.createElement('form');
+  form.setAttribute('method', 'post');
+  form.setAttribute('action', '#')
+  form.id = 'form';
+  form.name = "name";
+
+  var label = document.createElement('label');
+  label.innerHTML = "You seem to like our website! Why not sign up for our email list? We'll send you updates whenever something changes!";
+
+  var input = document.createElement('input');
+  input.id = 'email';
+  input.email = 'email';
+  input.name = 'email';
+  input.placeholder = 'email';
+  input.type = 'text';
+
+  var button = document.createElement('a');
+  button.id = 'submit';
+  button.innerHTML = "ok!"
+
+  // no painting yet
+  container.appendChild(form);
+  form.appendChild(label);
+  form.appendChild(input);
+  form.appendChild(button);
+}
 
 // Validating Empty Field
 function check_empty() {
